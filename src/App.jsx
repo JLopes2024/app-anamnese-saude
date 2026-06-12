2a53f8b244dd0842894f6ff2dcb23c31516e0272
JLopes2024 <jeffinho.je8@gmail.com>
2026-06-12 09:51:45 -0300

    Fix: ajustando nomes de arquivos para case-sensitive

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Header from './components/Header/Header';
 import Hero from './components/Hero/Hero';
 import Planner from './components/Planner/Planner';
 import Anamnese from './components/Anamnese/Anamnese';
+
 // Adicionei calcularDisponibilidadeNeuromuscular aqui
 import { gerarRecomendacao, obterOpcoesDisponiveis, gerarBriefing, calcularDisponibilidadeNeuromuscular } from './utils/logicaApp';
 import './styles/App.css';
